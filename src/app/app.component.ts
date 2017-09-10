d30309d1dc13ba866b3978da28f8da84f9b8c579
Miguel Breceda <miguel.breceda@outlook.es>
2017-09-10 18:12:19 -0500

    add: current user to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, DoCheck{
+  public title = 'app';
+  public user: string;
+
+  ngOnInit() {
+    this.user = localStorage.getItem('contactEmail');
+  }
+
+  ngDoCheck() {
+    this.user = localStorage.getItem('contactEmail');
+  }
+
 }
